c7fb118d15b055930945f994d81f9e2ff4f5dc19
chaimaa <cm3568@stern.nyu.edu>
2019-06-19 19:25:41 -0400

    Latest closing price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,6 +3,11 @@
 import requests
 import dotenv
 import json
+import datetime
+
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
 
 #
 # INFO INPUTS
@@ -17,8 +22,12 @@ response = requests.get("request_url")
 
 parsed_response = json.loads(response.text)
 
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_close = parsed_response["Time Series (Daily)"]["2019-02-20"]["4. close"]
+
 
 breakpoint()
+
 
 #
 # INFO OUTPUTS
@@ -28,10 +37,10 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print(f"REQUEST AT:" + now.strftime("%Y-%m-%d %H:%M:%S"))
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
