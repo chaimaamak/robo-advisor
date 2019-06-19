ac068411764e40b6b7e537ef66629d2a41643c0b
chaimaa <cm3568@stern.nyu.edu>
2019-06-19 19:46:56 -0400

    recent high price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -23,10 +23,32 @@ response = requests.get("request_url")
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-latest_close = parsed_response["Time Series (Daily)"]["2019-02-20"]["4. close"]
 
+tsd = parsed_response["Time Series (Daily)"]
+
+# assuming latest day is first, may want to sort to ensure latest day is first
+dates = list(tsd.keys())
+
+latest_day = dtaes[0]
+
+latest_close = tsd[latest_day]["4. close"]
 
 breakpoint()
+
+# get high price from each day
+high_prices =[]
+# maximum of all high prices
+for date in dates:
+    high_price = tsd[date]["2. high"]
+    high_prices.append(float(high_price))
+recent_high = max(high_prices)
+
+
+# minimum of all low prices
+low_prices =[]
+recent_low = min(low_prices)
+
+
 
 
 #
@@ -41,8 +63,8 @@ print(f"REQUEST AT:" + now.strftime("%Y-%m-%d %H:%M:%S"))
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
