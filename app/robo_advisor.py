d6dad0e63febd8e1f37d859681fbe8d619270b5b
chaimaa <cm3568@stern.nyu.edu>
2019-06-23 23:02:53 -0400

    Recommendation
    Recommendation logic based on recent close price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -6,20 +6,31 @@ import os
 
 import datetime
 import requests
-import dotenv
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price)
-
 
 #
 # INFO INPUTS
 #
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
+api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 
-response = requests.get("request_url")
+while True:
+    symbol = input("Please Enter a Valid Ticker Symbol: ")
+
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+
+    response = requests.get(request_url)
+
+    if "Error" in response.text:
+        print("This Symbol is Invalid. Please Try Again!")
+        break
+
 #print (type(response)) #> class 'request.models.Response'>
 #print (response.status_code) #>200
 #print (response.text)
@@ -33,31 +44,35 @@ tsd = parsed_response["Time Series (Daily)"]
 # assuming latest day is first, may want to sort to ensure latest day is first
 dates = list(tsd.keys())
 
-latest_day = dtaes[0]
+sorted_dates = sorted(dates, reverse=True)
+
+latest_day = dates[0]
 
 latest_close = tsd[latest_day]["4. close"]
-
-breakpoint()
 
 # get high price from each day
 high_prices =[]
 low_prices = []
+close_prices = []
+
 # maximum of all high prices
 for date in dates:
     high_price = tsd[date]["2. high"]
     high_prices.append(float(high_price))
     low_price = tsd[date]["3. low"]
     low_prices.append(float(low_price))
+    close_price = tsd[date]["4. close"]
+    close_prices.append (float(close_price))
+
 recent_high = max(high_prices)
 recent_low = min(low_prices)
-
+average_close = sum(close_prices)/len(close_prices)
 
 #
 # INFO OUTPUTS
 #
 
-
-csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", f"{symbol}.csv")
 
 csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
 
@@ -76,23 +91,35 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
             "volume": daily_prices["5. volume"]
         })
 
-
+#
+# INFO DISPLAY
+#
 
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print(f"SELECTED SYMBOL: {symbol}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print(f"REQUEST AT:" now.strftime("%Y-%m-%d %H:%M:%S"))
+print(f"REQUEST AT: {now.strftime("%Y-%m-%d %H:%M:%S")}")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
+
+#
+# RECOMMENDATION
+#
+
+if average_close > float(latest_close):
+    print("RECOMMENDATION: BUY!")
+    print("RECOMMENDATION REASON: The average close price is greater than the most recent close price")
+else:
+    print("RECOMMENDATION: SELL!")
+    print("RECOMMENDATION REASON: The stock is over valued. The average close price is lower than the most recent close price")
+
 print("-------------------------")
-print(f"WRITING DATA TO CSV: {csv_file_path}"")   # use csv module
+print(f"WRITING DATA TO CSV: {csv_file_path}")   # use csv module
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
